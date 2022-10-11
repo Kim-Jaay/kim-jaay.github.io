6a43097d04e0472d35e9a3b7b0bb0ba325f5007b
Kim-Jaay <jjjjuhyuneee@naver.com>
2022-10-11 09:36:19 +0900

    1011

--- document/js/smile.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $('.face li').on('click', function () {
-        var idx = $(this).index();
-        console.log(smile)
-        $('.face li').css({
-            top: 250
-        })
-    })
-})
--- react_00/project00/src/Nav.js
+++
@@ -1,9 +0,0 @@
-function Nav() {
-  return (
-    <div>
-      메뉴 입니다.
-    </div>
-  );
-}
-
-export default Nav;
