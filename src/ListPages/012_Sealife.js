fcfc59ced388cb0c9f45ddbf55a01dd15187da46
Jay <jjjjuhyuneee@naver.com>
2022-10-12 23:09:22 +0900

    1012

--- src/ListPages/012_Sealife.js
+++ src/ListPages/012_Sealife.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import LIST from '../data/List.js'
 
 const Sealife = () => {
     return (
         <div className="inner">
-            sealife
+            {
+                LIST.map((it, idx) => {
+                    return (
+                        <figure>
+                            <h2>{it.title}</h2>
+                            <p>{it.tools}</p>
+                        </figure>
+                    )
+                })
+                    .slice(11)
+            }
         </div>
     )
 }
