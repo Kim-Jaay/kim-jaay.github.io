7b65d8f655432a6bcf4d19da3abc03fd6b883dee
Kim-Jaay <jjjjuhyuneee@naver.com>
2022-10-14 16:31:26 +0900

    1014

--- src/ListPages/Nasa.js
+++ src/ListPages/Nasa.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
 import LIST from '../data/List';
+import '../css/Nasa.scss'
+
 const Nasa = () => {
     const { id } = useParams()
     const matchId = LIST.find(it => it.id == id);
     return (
-        <>
+        <div className='Nasa'>
             <div className="inner">
-                <img src={process.env.PUBLIC_URL + '/assets/img/sub12_00.png'} alt="" className='banner' />
+                <img src={process.env.PUBLIC_URL + `/assets/img/sub09_00.png`} alt="" className='banner' />
 
                 <div className="inner1200 subcon">
                     <h2>{matchId.title}</h2>
                     <p>{matchId.des2}</p>
                     <div className="img">
-                        <img src={process.env.PUBLIC_URL + '/assets/img/sub12_01.png'} alt="" className='img01' />
-                        <img src={process.env.PUBLIC_URL + '/assets/img/sub12_02.gif'} alt="" className='img02' />
-                        <img src={process.env.PUBLIC_URL + '/assets/img/sub12_03.png'} alt="" className='img03' />
+                        <img src={process.env.PUBLIC_URL + '/assets/img/sub09_01.png'} alt="" className='img01' />
+                        <img src={process.env.PUBLIC_URL + '/assets/img/sub09_02.png'} alt="" className='img02' />
+                        <img src={process.env.PUBLIC_URL + '/assets/img/sub09_03.gif'} alt="" className='img03' />
                     </div>
 
                     <div className="bottom">
@@ -28,7 +30,7 @@ const Nasa = () => {
             </div>
 
 
-        </>
+        </div>
     )
 }
 
