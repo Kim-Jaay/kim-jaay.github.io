5e0d3a3347d9e8c5a22939187b44dece7eb89124
Jay <jjjjuhyuneee@naver.com>
2022-10-19 23:07:09 +0900

    1019

--- src/ListPages/Oncheon.js
+++ src/ListPages/Oncheon.js
@@ -1,23 +1,64 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
 import LIST from '../data/List';
+import '../css/Oncheon.scss'
+
 const Oncheon = () => {
     const { id } = useParams()
     const matchId = LIST.find(it => it.id == id);
     return (
-        <>
+        <div className='Oncheon'>
             <div className="inner">
-                <img src={process.env.PUBLIC_URL + '/assets/img/sub12_00.png'} alt="" className='banner' />
+                <img src={process.env.PUBLIC_URL + '/assets/img/sub02_00.png'} alt="" className='banner' />
 
                 <div className="inner1200 subcon">
                     <h2>{matchId.title}</h2>
                     <p>{matchId.des2}</p>
-                    <div className="img">
-                        <img src={process.env.PUBLIC_URL + '/assets/img/sub12_01.png'} alt="" className='img01' />
-                        <img src={process.env.PUBLIC_URL + '/assets/img/sub12_02.gif'} alt="" className='img02' />
-                        <img src={process.env.PUBLIC_URL + '/assets/img/sub12_03.png'} alt="" className='img03' />
-                    </div>
+                </div>
 
+                <div className="inner_sec">
+                    <div className="inner1200 subcon">
+                        <h2>{matchId.title}</h2>
+                        <p>{matchId.des2}</p>
+                    </div>
+                    <img src={process.env.PUBLIC_URL + '/assets/img/sub02_01.png'} className='img01' />
+                </div>
+
+                <div className="inner_sec overview">
+                    <div className="inner1200 subcon">
+                        <h2>{matchId.title}</h2>
+                        <p>{matchId.des2}</p>
+                    </div>
+                </div>
+
+                <div className="inner_sec">
+                    <div className="inner1200 subcon">
+                        <h2>{matchId.title}</h2>
+                        <p>{matchId.des2}</p>
+                        <img src={process.env.PUBLIC_URL + '/assets/img/sub02_03.png'} />
+                    </div>
+                </div>
+                <img src={process.env.PUBLIC_URL + '/assets/img/sub02_04.png'} />
+                <div className="inner_sec">
+                    <div className="inner1200 subcon">
+                        <h2>{matchId.title}</h2>
+                        <p>{matchId.des2}</p>
+                        <img src={process.env.PUBLIC_URL + '/assets/img/sub02_05.png'} />
+                    </div>
+                </div>
+                <img src={process.env.PUBLIC_URL + '/assets/img/sub02_06.png'} />
+                <img src={process.env.PUBLIC_URL + '/assets/img/sub02_07.png'} />
+                <div className='image'>
+                    <img src={process.env.PUBLIC_URL + '/assets/img/sub02_08.png'} />
+                    <img src={process.env.PUBLIC_URL + '/assets/img/sub02_09.png'} />
+                </div>
+
+
+
+
+
+
+                <div className="inner1200 subcon">
                     <div className="bottom">
                         <h3>{matchId.title}</h3>
                         <p>{matchId.field}</p>
@@ -28,7 +69,8 @@ const Oncheon = () => {
             </div>
 
 
-        </>
+
+        </div >
     )
 }
 
