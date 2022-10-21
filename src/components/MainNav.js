e40592870ce8f924bb46ce037a803024cc580232
Jay <jjjjuhyuneee@naver.com>
2022-10-21 22:33:02 +0900

    1021

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -4,13 +4,19 @@ import NAV from '../data/Nav.js'
 
 const MainNav = () => {
     return (
-        <ul className="main_nav">{
-            NAV.map((it, idx) => {
-                return (
-                    <li><Link to={it.link}>{it.menu}</Link></li>
-                )
-            })
-        }
+        <ul className="main_nav">
+            <li><a href="https://github.com/Kim-Jaay" target='_blank'><i className="xi-github"></i></a></li>
+            <li><a href="https://namecard.kakao.com/kimjay" target='_blank'><i className="xi-kakaotalk"></i></a></li>
+            {/* {
+                NAV.map((it, idx) => {
+                    return (
+                        <li>
+                            <a href={it.link}>{it.menu}</a>
+
+                        </li>
+                    )
+                })
+            } */}
         </ul>
     )
 }
