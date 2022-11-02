8b455e50ec68d28ea4d40b16e18813064063bd0e
Jay <jjjjuhyuneee@naver.com>
2022-11-02 17:00:34 +0900

    1102

--- src/ListPages/Louvre.js
+++ src/ListPages/Louvre.js
@@ -128,12 +128,12 @@ const Louvre = () => {
     return (
         <div className='Louvre'>
             <div className="inner">
-                <img src={process.env.PUBLIC_URL + '/assets/img/sub11_00.jpg'} alt="" className='banner' />
+                <img src={process.env.PUBLIC_URL + '/assets/img/sub12_00.jpg'} alt="" className='banner' />
                 <div className="inner1200 subcon">
                     <h2>{matchId.title}</h2>
                     <p>{matchId.des2}</p>
-                    <img src={process.env.PUBLIC_URL + '/assets/img/sub11_01.png'} alt="" className='img01' />
-                    <img src={process.env.PUBLIC_URL + '/assets/img/sub11_02.png'} alt="" className='img02' />
+                    <img src={process.env.PUBLIC_URL + '/assets/img/sub12_01.png'} alt="" className='img01' />
+                    <img src={process.env.PUBLIC_URL + '/assets/img/sub12_02.png'} alt="" className='img02' />
 
 
                     <h2>Check the Code : React</h2>
