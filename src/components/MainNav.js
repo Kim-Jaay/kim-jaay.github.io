e38009948437da2f7e91963b730947ed56f6b10c
Kim-Jaay <jjjjuhyuneee@naver.com>
2022-11-04 14:39:49 +0900

    1104

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -5,9 +5,9 @@ import NAV from '../data/Nav.js'
 const MainNav = () => {
     return (
         <ul className="main_nav">
-            <li><a href="https://github.com/Kim-Jaay" target='_blank'><i className="xi-github"></i> GitHub</a></li>
-            <li><a href="https://namecard.kakao.com/kimjay" target='_blank'><i className="xi-kakaotalk"></i> Kakao</a></li>
-            <li><a href={process.env.PUBLIC_URL + '/assets/img/Resume_KimJuHyun.pdf'} download="Resume_김주현"><i className="xi-folder-download"></i> Resume</a></li>
+            <li><a href="https://github.com/Kim-Jaay" target='_blank'><i className="xi-github"></i> <p>GitHub</p></a></li>
+            <li><a href="https://namecard.kakao.com/kimjay" target='_blank'><i className="xi-kakaotalk"></i> <p>Kakao</p></a></li>
+            <li><a href={process.env.PUBLIC_URL + '/assets/img/Resume_KimJuHyun.pdf'} download="Resume_김주현"><i className="xi-folder-download"></i> <p>Resume</p> </a></li>
             {/* {
                 NAV.map((it, idx) => {
                     return (
