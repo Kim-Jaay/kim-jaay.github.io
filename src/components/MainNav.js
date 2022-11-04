3df17d9d15d3e6464874206b9634236956baee1f
Kim-Jaay <jjjjuhyuneee@naver.com>
2022-11-04 14:16:40 +0900

    1104

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -7,6 +7,7 @@ const MainNav = () => {
         <ul className="main_nav">
             <li><a href="https://github.com/Kim-Jaay" target='_blank'><i className="xi-github"></i></a></li>
             <li><a href="https://namecard.kakao.com/kimjay" target='_blank'><i className="xi-kakaotalk"></i></a></li>
+            <li><a href={process.env.PUBLIC_URL + '/assets/img/Resume_KimJuHyun.pdf'} download="Resume_Juhyun"><i className="xi-folder-download"></i></a></li>
             {/* {
                 NAV.map((it, idx) => {
                     return (
